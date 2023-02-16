d9ee7f06d9863f75cf4384e69a17ab453a817587
evelynmaduagu <maduaguevelyn555@yahoo.com>
2023-02-16 15:53:14 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/**
+ * main . A program that prints a line with puts a line with puts functions
+ * Return: O (Success)
+ */
+Int main(void)
+{
+Put(""Programming is like building a multilingual puzzle, followed by a new line");
+} return (0);
