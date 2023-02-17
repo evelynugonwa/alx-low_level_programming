5dd0a000d3c17e7cc0152f8c51cf13650e382fc0
evelynmaduagu <maduaguevelyn555@yahoo.com>
2023-02-16 16:34:15 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,53 @@
+#Include <stdio.o>
+/**
+ * main - A program that prints a line with puts function
+ *Return: 0 (Success)
+ */
+Int main (void)
+{
+printf("with with proper grammar, but the outcome is a piece of art,\n"}
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
