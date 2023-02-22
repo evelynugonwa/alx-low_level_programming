b96c241f9ec13fe7b701aad198c548a8344d7610
evelynmaduagu <maduaguevelyn555@yahoo.com>
2023-02-22 02:48:44 -0800

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++
@@ -1,8 +0,0 @@
-/**
- * main . A program that prints a line with puts a line with puts functions
- * Return: O (Success)
- */
-Int main(void)
-{
-Put(""Programming is like building a multilingual puzzle, followed by a new line");
-} return (0);
