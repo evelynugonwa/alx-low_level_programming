ec9a80946161d590a132003e4617794a14d937fd
evelynmaduagu <maduaguevelyn555@yahoo.com>
2023-02-22 13:10:53 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+        char c = 'a';
+
+        while (c = 'a'; c <= 'z'; c++)
+        {
+                _putchar(c);
+		c++;
+        }
+
+        _putchar('\n');
+}
+
